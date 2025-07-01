e8a4b796ac6d70441650769821c92c1327b36e8b
Denis <kripmrx@gmail.com>
2025-07-01 13:36:58 +0300

    docs(Swagger): Configure swagger

---
+++ backend/src/swagger.ts
@@ -0,0 +1,24 @@
+import swaggerJsdoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
+
+const options: swaggerJsdoc.Options = {
+  swaggerDefinition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Swagger Express API',
+      version: '1.0.0',
+      description: 'A simple Express API with Swagger documentation',
+    },
+    servers: [
+      {
+        url: 'http://localhost:3000',
+        description: 'Local dev sever',
+      }
+    ],
+  },
+  apis: ['./src/routes/*.ts'],
+};
+
+const specs = swaggerJsdoc(options);
+
+export { specs, swaggerUi };
