f27e9aa336ec7eb62b1c1de4e894999b87cded95
Denis <kripmrx@gmail.com>
2025-07-01 13:28:37 +0300

    chore(env): Parse env variables

---
+++ backend/src/config/config.ts
@@ -0,0 +1,27 @@
+import dotenv from 'dotenv';
+import { z } from 'zod';
+
+dotenv.config();
+
+// Schema for env variables
+const envSchema = z.object({
+  PORT: z.string().default('3000'),
+  DB_URL: z.string().url({
+    message: 'DB_URL must have valid url',
+  }),
+});
+
+const _env = envSchema.safeParse(process.env);
+
+if (!_env.success) {
+  console.error(' Invalid environment variables:', _env.error.format())
+  process.exit(1)
+}
+
+const env = _env.data
+
+// Typed config object
+export const config = {
+  PORT: Number(env.PORT),
+  DB_URL: env.DB_URL,
+}
