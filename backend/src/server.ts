111ecf9621a581c6d714ea7c345783987fb8a0eb
Denis <kripmrx@gmail.com>
2025-07-09 17:32:07 +0300

    chore(Backend): add cors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import { router } from './routes/users';
 import { swaggerUi, specs } from './swagger';
 import { errorHandler } from './middlewares/errorHandler';
@@ -7,6 +8,12 @@ import { errorHandler } from './middlewares/errorHandler';
 const app = express();
 
 app.use(express.json());
+app.use(cors({
+  origin: 'http://localhost:5173', 
+  credentials: true, 
+}));
+
+
 
 app.use('/users', router);
 
