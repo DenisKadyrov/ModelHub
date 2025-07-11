e46fd9f30c038d5ca8f78f77a160284fc341fb87
Denis <kripmrx@gmail.com>
2025-07-11 15:27:23 +0300

    feat(api): Login api function

--- frontend/src/api/users.ts
+++ frontend/src/api/users.ts
@@ -13,7 +13,21 @@ interface RegisterResponse {
   };
 }
 
+interface LoginData {
+  email: string;
+  password: string
+}
+
+interface LoginResponse {
+  token: string;
+}
+
 export const register = async (data: RegisterData): Promise<RegisterResponse> => {
   const response = await api.post<RegisterResponse>('/users/signup', data);
+  return response.data;
+};
+
+export const login = async (data: LoginData): Promise<LoginResponse> => {
+  const response = await api.post<LoginResponse>('/users/login', data);
   return response.data;
 };
