af57f0e8c8479279a57676a7b92bd2da10ecb2dd
Denis <kripmrx@gmail.com>
2025-07-11 15:29:38 +0300

    feat(router): add login route

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,10 +1,13 @@
 import { Routes, Route } from 'react-router-dom';
 import Register from '../pages/Register';
+import Login from '../pages/Login';
 
 export default function AppRoutes() {
   return (
     <Routes>
       <Route path="/register" element={<Register />} />
+      <Route path='/login' element={<Login />} />
+      <Route path="*" element={<Login />} />
     </Routes>
   );
 }
