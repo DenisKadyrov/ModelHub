92bc8312093f4f70648c4b1fcb27d6acd5dcbf1a
Denis <kripmrx@gmail.com>
2025-07-11 15:25:12 +0300

    refactor(Pages): Register page using From Wrapper

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -1,19 +1,15 @@
 import RegisterForm from "../components/RegisterForm";
+import FormWrap from "../components/FormWrap";
 
 export default function Register() {
   return (
     <>
-      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8" >
-        <div className="sm:mx-auto sm:w-full sm:max-w-sm" >
-          {
-            // some logo
-          }
-          <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900" >
-            Create new account
-          </h2>
-          <RegisterForm />
-        </div>
-      </div>
+      <FormWrap >
+        <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900" >
+          Create new account
+        </h2>
+        <RegisterForm />
+      </FormWrap>
     </>
   );
 }
