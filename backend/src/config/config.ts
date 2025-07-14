616d71943a021210bb8f634a328581ab8fbefaad
Denis <kripmrx@gmail.com>
2025-07-14 22:32:55 +0500

    chore(MinIO): getting minio env variables

--- backend/src/config/config.ts
+++ backend/src/config/config.ts
@@ -10,6 +10,12 @@ const envSchema = z.object({
     message: 'DB_URL must have valid url',
   }),
   JWT_SECRET: z.string(),
+  MINIO_ENDPOINT: z.string(),
+  MINIO_PORT: z.number().default(9003),
+  MINIO_USE_SSL: z.boolean(),
+  MINIO_ACCESS_KEY: z.string(),
+  MINIO_SECRET_KEY: z.string(),
+  BUCKET_NAME: z.string(),
 });
 
 const _env = envSchema.safeParse(process.env);
@@ -26,4 +32,10 @@ export const config = {
   PORT: Number(env.PORT),
   DB_URL: env.DB_URL,
   JWT_SECRET: env.JWT_SECRET,
+  MINIO_ENDPOINT: env.MINIO_ENDPOINT,
+  MINIO_PORT: env.MINIO_PORT,
+  MINIO_USE_SSL: env.MINIO_USE_SSL,
+  MINIO_ACCESS_KEY: env.MINIO_ACCESS_KEY,
+  MINIO_SECRET_KEY: env.MINIO_SECRET_KEY,
+  MINIO_BUCKET_NAME: env.BUCKET_NAME,
 }
