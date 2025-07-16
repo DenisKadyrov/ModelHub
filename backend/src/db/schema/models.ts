c1bf49c3a37d4a17e1809ff736d91ace15a885f3
Denis <kripmrx@gmail.com>
2025-07-16 18:01:11 +0500

    fix(schema): Add path to model schema in db

--- backend/src/db/schema/models.ts
+++ backend/src/db/schema/models.ts
@@ -1,12 +1,13 @@
 import { integer, varchar, serial, pgTable } from 'drizzle-orm/pg-core';
 import { relations } from 'drizzle-orm';
 import { timestamps } from '../column.helpers';
-import { usersTable } from './users.ts';
+import { usersTable } from './users';
 
 export const modelsTable = pgTable('models', {
   id: serial('id').primaryKey(),
   userId: integer('user_id').references(() => usersTable.id),
   name: varchar('name', { length: 255 }).notNull(),
+  path: varchar('path', { length: 1024 }).notNull(),
   description: varchar('description', { length: 600 }).notNull(),
   framework: varchar('framework', { length: 255 }).notNull(),
   ...timestamps,
