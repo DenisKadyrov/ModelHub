4ca7141a9ead09b2b572b3aaf2df0bbd7389a957
Denis <kripmrx@gmail.com>
2025-07-17 20:10:34 +0500

    feat(Routes): connect model router and use aliases

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
-import { router } from './routes/users';
+import { router as userRouter } from './routes/users';
+import { router as modelRouter } from './routes/models';
 import { swaggerUi, specs } from './swagger';
 import { errorHandler } from './middlewares/errorHandler';
 
@@ -9,13 +10,14 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-  origin: 'http://localhost:5173', 
-  credentials: true, 
+  origin: 'http://localhost:5173',
+  credentials: true,
 }));
 
 
 
-app.use('/users', router);
+app.use('/users', userRouter);
+app.use('/models', modelRouter);
 
 app.use(
   '/api-docs',
