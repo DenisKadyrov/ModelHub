e42a06148507050c3483aec033691a3b8ae8f552
Denis <kripmrx@gmail.com>
2025-07-17 17:24:59 +0500

    feature(MinIO): upload model service

---
+++ backend/src/services/models.ts
@@ -0,0 +1,39 @@
+import { minioClient } from '../config/minio';
+import { config } from '../config/config';
+import { createModel } from '../repositories/models';
+import { v4 as uuidv4 } from 'uuid';
+
+export const uploadModel = async (data: {
+  userId: number,
+  name: string,
+  description: string,
+  framework: string,
+  file: Express.Multer.File,
+}) => {
+  const extension = data.file.originalname.split('.').pop();
+  const filename = `${uuidv4()}.${extension}`;
+  const path = `models/${filename}`;
+
+  // Upload to MinIO
+  await minioClient.putObject(
+    config.MINIO_BUCKET_NAME,
+    path,
+    data.file.buffer,
+    data.file.buffer.length,
+    {
+      'Content-Type': data.file.mimetype,
+    }
+  );
+
+
+  // Save metadata to DB
+  const [record] = await createModel({
+    name: data.name,
+    filename: filename,
+    description: data.description,
+    userId: data.userId,
+    framework: data.framework,
+    path,
+  });
+  return record;
+};
