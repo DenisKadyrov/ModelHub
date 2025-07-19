fd0c93aad9f48d6883ccf17cb72c89e3a3453233
Denis <kripmrx@gmail.com>
2025-07-19 09:24:15 +0500

    refactor: delete some comments

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3000', // или .env
-  withCredentials: true, // если используешь куки
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3000',
+  withCredentials: true,
 });
 
 export default api;
